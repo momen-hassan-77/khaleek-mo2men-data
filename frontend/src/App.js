f374826a5ce5310907a801af9a08202cf577c69d
momen-hassan-77 <momensalman77@gmail.com>
2025-07-07 00:10:52 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import Navbar from './components/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App;
